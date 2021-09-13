e47f1e1633f4221f6701d48a60ad82cef2cc96f0
Flavio Santos <flavioanalistadesistema@gmail.com>
2021-09-13 10:29:12 -0300

    feat: exercicies 3) expente

---
+++ exercicios/exec_3.js
@@ -0,0 +1,9 @@
+/*
+03)​ Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+*/
+
+function params(base, expoent) {
+    return base ** expoent;
+}
+
+console.log(params(5, 3)); // resultado 125
